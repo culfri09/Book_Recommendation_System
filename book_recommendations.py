41ef39e174d5b15383582564508abba891ce0e39
Marta Lendínez <95087997+culfri09@users.noreply.github.com>
2023-10-17 19:54:25 +0200

    Comment code of the python file

--- book_recommendations.py
+++ book_recommendations.py
@@ -3,59 +3,60 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-# Your recommendation code here
+'''
+---Recommendation Algorithm---
+'''
 
-# Cargar el conjunto de datos
-df = pd.read_csv('books.csv') #data frame: es un objeto que obtiene todo el archivo
+# Loads data set into data frame.
+df = pd.read_csv('books.csv') 
 
-# Preprocesamiento de datos
-# Crean una mega matriz en la que cada palabra esta representada por un numero, cuyo peso depende de la frecuencia, exceptuando las stop words.
-tfidf_vectorizer = TfidfVectorizer(stop_words='english') #Palabras frecuentes sin importancia del idioma inglés (stop words)
-tfidf_matrix = tfidf_vectorizer.fit_transform(df['description'].fillna('')) # Convierte las palabras del .csv en números.unicamente usa el campo descripción.
 
-# Calcular similitudes entre libros usando el producto interno del coseno
-cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix) #Lista que contiene las distancias entre descripciones de libros (de la matriz). Distancias entre todos los alementos
+#Data preprocessing: creates a huge matrix in which every word is represented by a number (the weight depends in the frequency). Stop words are not included.
+tfidf_vectorizer = TfidfVectorizer(stop_words='english') #Initializes the TfidfVectorizer with stop words removed.
+tfidf_matrix = tfidf_vectorizer.fit_transform(df['description'].fillna('')) # Fits and transform the data set from the .csv (just the description) to create the matrix.
 
-print(type(cosine_similarities))
-# Función para obtener las recomendaciones de libros
-# Inputeas el título del libro + numero de recomendaciones que tienes
+# Computes cosine similarities
+cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix) # List which contains the distance between ALL the book's descriptions within the matrix. 
+
+#print(type(cosine_similarities))
+
+# Method to obtain the book recommendations. Takes as parameters the book's name and the number of recommendations that will be given. 
 def get_recommendations(book_title, num_recommendations=5):
-    book_index = df.index[df['title'] == book_title].tolist() #Encuentra al libro en base a su posición de la lista. 
+    book_index = df.index[df['title'] == book_title].tolist() # Finds the index of the book within the list. 
     if not book_index:
-        return [] #Te devuelve una lista de recumenadción vacía.
+        return [] #If title is not within the df, it returns an empty recommendation.
     
     book_index = book_index[0]
     
-    # Obtener las puntuaciones de similitud de este libro con otros libros
-    sim_scores = list(enumerate(cosine_similarities[book_index])) #Crea una lista con todas las distancias entre nuestro libro y los del df.
-    
-    # Ordenar los libros por similitud
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True) #Ordena la lista de distancias de mayor a menor.
-    
-    # Obtener las puntuaciones de similitud de los libros recomendados
-    recommendations = sim_scores[1:num_recommendations+1] #Crea una lista de recomendaciones (numeros) con los primeros 5 elementos de la lista de distancias.
-    
-    # Obtener los títulos de los libros recomendados
-    recommended_books = [(df['title'][rec[0]], rec[1]) for rec in recommendations] #Convierte los numeros de recommendations que representnas las recomendaciones a strings y los mete en una lista
-    
-    return recommended_books #lista con tuplas (titulo + distancia)
+    # Obtains the similitude scores between the input book with the rest of the books in the df. 
+    sim_scores = list(enumerate(cosine_similarities[book_index])) #Creates a list with all of the distances between the input book and the ones in the df.
 
-# Ejemplo de recomendación
-#book_title = "Pride and Prejudice"
-#recommendations = get_recommendations(book_title)
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True) # Sorts books based on similitude score (from highest to lowest).
+    
+    recommendations = sim_scores[1:num_recommendations+1] #Creates list with the first 5 items of the sim_scores (list with the sorted books by similitude)
+    
+    recommended_books = [(df['title'][rec[0]], rec[1]) for rec in recommendations] #Transforms the numbers of the recommendations into strings and adds them to recommendation_books list.
+    
+    return recommended_books #List with tuples (title + distance).
 
+'''
+---Flask integration--
+'''
 
-app = Flask(__name__, template_folder='./template')
+app = Flask(__name__, template_folder='./template') #Creates a Flask application named app, and specifies path where the rendered HTML pages are located.
 
+#Loads main page when user accesses the root of the URL.
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#Loads recommendation page when user inputs a recommendation.
 @app.route('/recommend', methods=['POST'])
 def recommend():
     book_title = request.form['book_title']
     recommendations = get_recommendations(book_title)
     return render_template('recommendations.html', recommendations=recommendations)
 
+#Starts web server, and web application becomes accesible in the specified port.
 if __name__ == '__main__':
     app.run(port=4996)
